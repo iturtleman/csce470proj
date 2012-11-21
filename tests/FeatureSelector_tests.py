3dbada892e1066e77533f9e8d1e5576df0a3d55d
kyoko <kyoko@kyoko-1005HA.(none)>
2012-11-20 22:20:54 -0600

    implemented chi test

--- tests/FeatureSelector_tests.py
+++ tests/FeatureSelector_tests.py
@@ -14,7 +14,14 @@ def read_tweets(filename):
 
 TREND_CORPUS = read_tweets('trending.json')
 NONTREND_CORPUS = read_tweets('nontrending.json')
-
+chi_values = {'via' : 1.5, 'twitter' : 1.5, 'the' : 1.5, 'party' : 1.5, 'martes' : 1.5, 
+    'join' : 1.5, 'feliz' : 1.5, 'est' : 1.5, '@stacieinatlanta' : 1.5, '2pm' : 1.5,
+    '16' : 1.5, '11' : 1.5, '1' : 1.5, '#disneykmart' : 1.5, 'tambi' : 0.6, 'si' : 0.6,
+    'rt' : 0.6, 'n' : 0.6, 'muy' : 0.6, 'lqm' : 0.6, 'http://t.co/yeshpffz' : 0.6,
+    'http://t.co/mb6lrqh0' : 0.6, 'http://t.co/7xevrait' : 0.6, 'haces' : 0.6,
+    'esto' : 0.6, 'en' : 0.6, 'clase' : 0.6, 'bellas' : 0.6, '@silviaarafans' : 0.6,
+    '@lapatronatv' : 0.6, '@aracelymridaf' : 0.6, '@aracelyfans_usa' : 0.6,
+    '@aracelyfans_mex' : 0.6, '@aracely_fans' : 0.6, '@amada1989' : 0.6}
 
 class TestFeatureSelection(unittest.TestCase):
     def setUp(self):
@@ -23,7 +30,9 @@ class TestFeatureSelection(unittest.TestCase):
         
     # add tests here
     def test_feature_selector(self):
-        self.featureSelector.select('asdf')
+        x = self.featureSelector.select('asdf')
+        for key,chi in x.items():
+            self.assertAlmostEqual(chi, chi_values[key])
         pass
 
 if __name__ == '__main__':
